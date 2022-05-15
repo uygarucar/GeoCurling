b5f0c01cb1f4d5c509cc085df3d94da9482a8830
Uygar Uçar <uygarucar2@gmail.com>
2022-05-15 22:56:53 +0300

    bug fix

--- src/Modules/Play/Utils/DispatchCoordinates.js
+++ src/Modules/Play/Utils/DispatchCoordinates.js
@@ -5,10 +5,10 @@ import useDispatchTarget from "../CustomHooks/useDispatchTarget";
 const dispatchCoordinateForChosenTopic = (topic) => {
     let coordinate;
      switch (topic) {
-        case topic: 1
+        case 1:
             coordinate = GiveCoordinates(TargetPlaces_Unit1)
             break;
-        case topic: 2
+        case 2:
             coordinate = GiveCoordinates(TargetPlaces_Unit2)
             break;
         default:

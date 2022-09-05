79a804e0f43d0bfe2aa6f8d16a1cd2440b4c18d8
Uygar Uçar <uygarucar2@gmail.com>
2022-09-05 12:21:42 +0300

    Unit2 completed except coords

---
+++ src/Modules/Play/Data/TargetPlacesInfos_Unit2.js
@@ -0,0 +1,183 @@
+export const Ovalar = [
+    {//Bursa
+        id: 1,
+        text: 'Dağ eteği ovası hangi şehrimizdedir?',
+        latitude: 39.71944,
+        longitude: 43.05139
+    },
+    {//Muş
+        id: 2,
+        text: 'Dağ içi ovası hangi şehrimizdedir?',
+        latitude: 36.7477,
+        longitude: 36.3327
+    }
+    ,
+    {//Burdur, Isparta, Antalya tarafları
+        id: 3,
+        text: 'Karstik bir ova nerede olabilir? (Gölovası olarak da sorabilirsin.)',
+        latitude: 40.587580134,
+        longitude: 37.178795716,
+    },
+    {
+        //Adana???
+        id: 4,
+        text: 'En verimli ova',
+        latitude: 40.587580134,
+        longitude: 37.178795716,
+    },
+]
+export const AkarsuSekilleri = [
+    {
+        //Erzurum Narman
+        id: 1,
+        text: 'Kırmızı peripacalarının bulunduğu yer?',
+        latitude: 37.6889,
+        longitude: 43.1047
+    },
+    {
+        //Van
+        id: 2,
+        text: 'En fazla ülkeye sınırı olan bölgemizdeki peri bacaları hangi ilimizde yer alır',
+        latitude: 37.688716932396964,
+        longitude: 33.63490922641875
+    },
+    {
+        //Aydın
+        id: 3,
+        text: 'Birikinti konisi üzerinde kurulan şehrimiz',
+        latitude: 39.71944,
+        longitude: 43.05139
+    },
+    
+]
+export const Selalelerimiz = [
+    {//Kurşunlu Şelalesi
+        id: 1,
+        text: 'Kurşunlu şelalesi nerededir?',
+        latitude: 38.254,
+        longitude: 27.134
+    },
+    {//Kayseri
+        id: 2,
+        text: 'Kapuzbaşı şelalesi nerededir?',
+        latitude: 38.40115,
+        longitude: 42.10784
+    }
+    ,
+    {//Erzincan
+        id: 3,
+        text: 'Girlevik Şelalesi nerededir?',
+        latitude: 36.077286,
+        longitude: 32.832890,
+    }
+    ,
+    {//Sinop
+        id: 4,
+        text: 'Tatlıca Şelalesi nerededir?',
+        latitude: 36.077286,
+        longitude: 32.832890,
+    }
+    ,
+    {//Rize
+        id: 5,
+        text: 'Bulut Şelalesi nerededir?',
+        latitude: 36.077286,
+        longitude: 32.832890,
+    }
+]
+
+export const RuzgarSekilleri = [
+    {
+        //Iğdır
+        id: 1,
+        text: 'En fazla yükseltinin olduğu bölgemizde yer alan rüzgar şekillendirmesinin fazla olduğu il',
+        latitude: 40.945317723668275,
+        longitude: 29.5579235653009
+    },
+    {   //Kayseri Develi
+        id: 2,
+        text: 'En eski ticaret şehrinin bulunduğu bölgemizde yer alan rüzgar şekillendirmesinin bulunduğu yer?',
+        latitude: 39.755630120802216 ,
+        longitude: 34.84885581785901
+    }
+    ,
+    {//Nevşehir
+        id: 3,
+        text: 'Şahit Kaya ve Mantar kaya rüzgar şekillendirmesi türlerinin bulunduğu yerimiz?',
+        latitude: 37.606307017772515,
+        longitude: 33.59706017738192,
+    }
+    ,
+    {//Manisa
+        id: 3,
+        text: 'Ege bölgesinde yer alan kara kumulu bulunan ilimiz',
+        latitude: 37.606307017772515,
+        longitude: 33.59706017738192,
+    }
+]
+
+export const KarstikSekiller1 = [
+    {
+        //Çankırı 
+        id: 1,
+        text: 'Kaya tuzunun bulunduğu il',
+        latitude: 39.74588032538926, 
+        longitude: 39.479199134379584
+    },
+    {
+        //Ankara, Sivas
+        id: 2,
+        text: 'Jips/Alçı Taşının bulunduğu bir iç anadolu bölgesi ili',
+        latitude: 38.885349 ,
+        longitude: 40.498291
+    }
+    ,
+    {
+        //Burdur
+        id: 3,
+        text: 'İnsuyu mağarasının yer aldığı ilimiz?',
+        latitude: 36.81110790740455,
+        longitude: 34.64146149178453,
+    }, 
+    {
+        //Tokat
+        id: 4,
+        text: 'Ballıca mağarasının yer aldığı ilimiz?',
+        latitude: 36.81110790740455,
+        longitude: 34.64146149178453,
+    }, 
+]
+
+
+export const KarstikSekiller2 = [
+    {
+        //Mersin
+        id: 1,
+        text: 'Cennet ve Cehennem obruklarının yer aldığı il',
+        latitude: 41.0255,
+        longitude: 40.5177
+    },
+    {
+        //Hatay Hassa
+        id: 2,
+        text: 'Karstik oluşumlu olmayan tek mağara',
+        latitude: 38.545910,
+        longitude: 44.185593
+    },
+    {
+        //Antalya
+        id: 3,
+        text: 'Traverten platosu üzerine kurulan kent',
+        latitude: 39.71944,
+        longitude: 43.05139
+    },
+    {
+        //Konya ovası
+        id: 4,
+        text: 'Yer altı sularının sondajla çekilmesiyle oluşan obrukların bulunduğu ilimiz',
+        latitude: 39.71944,
+        longitude: 43.05139
+    }
+]
+
+
--- src/Modules/Play/Utils/DispatchCoordinates.js
+++ src/Modules/Play/Utils/DispatchCoordinates.js
@@ -1,4 +1,5 @@
-import { Daglar, MasifAraziler, Platolar, Volkanizma, FayHatları } from '../Data/TargetPlacesInfos_Unit1_1'
+import { Daglar, MasifAraziler, Platolar, Volkanizma, FayHatları } from '../Data/TargetPlacesInfos_Unit1'
+import { Ovalar, AkarsuSekilleri, Selalelerimiz, RuzgarSekilleri, KarstikSekiller1, KarstikSekiller2 } from '../Data/TargetPlacesInfos_Unit2';
 import GiveCoordinates from "./GiveCoordinates";
 import useDispatchTarget from "../CustomHooks/useDispatchTarget";
 
@@ -21,12 +22,34 @@ const dispatchCoordinateForChosenTopic = (outerCategoryId, innerCategoryId) => {
                 coordinate = GiveCoordinates(Platolar)
                 break;
             }
-            else if(innerCategoryId== 5){
-                coordinate= GiveCoordinates(FayHatları)
+            else if (innerCategoryId == 5) {
+                coordinate = GiveCoordinates(FayHatları)
                 break;
             }
-
-        //case 2:
+        case 2:
+            if (innerCategoryId == 1) {
+                coordinate = GiveCoordinates(Ovalar)
+                break;
+            } else if (innerCategoryId == 2) {
+                coordinate = GiveCoordinates(AkarsuSekilleri)
+                break;
+            }
+            else if (innerCategoryId == 3) {
+                coordinate = GiveCoordinates(Selalelerimiz)
+                break;
+            }
+            else if (innerCategoryId == 4) {
+                coordinate = GiveCoordinates(RuzgarSekilleri)
+                break;
+            }
+            else if (innerCategoryId == 5) {
+                coordinate = GiveCoordinates(KarstikSekiller1)
+                break;
+            }
+            else if (innerCategoryId == 6) {
+                coordinate = GiveCoordinates(KarstikSekiller2)
+                break;
+            }
 
         default:
             break;

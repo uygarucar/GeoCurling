1c32dd80e943171c56c3c7733cff341ba6364d4f
Uygar Uçar <uygarucar2@gmail.com>
2022-09-09 01:51:09 +0300

    Modal improved

--- src/Modules/Play/Components/WhenGameUnitEnded.js
+++ src/Modules/Play/Components/WhenGameUnitEnded.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import { Metrics } from '../../../StylingConstants'
 
 
@@ -16,24 +16,33 @@ const WhenGameUnitEnded = (props) => {
         <View style={{
             flex: 1,
             width: Metrics.width * 0.7,
-            height: Metrics.height * 0.15,
+            height: Metrics.height * 0.35,
             borderRadius: 5,
             backgroundColor: '#A59F95',
             padding: Metrics.width * 0.05,
             marginHorizontal: Metrics.width * 0.1,
-            marginVertical: Metrics.width * 0.7
+            marginVertical: Metrics.width * 0.4
         }}>
             {passed ?
                 <>
-                    <Text>GEÇTİN</Text>
-                    <Text></Text>
-                    <Text> Puanın={'>'} {props.totalScore}</Text>
+                    <Text style={{fontSize: 24, alignSelf:'center', color:'green'}}>GEÇTİN</Text>
+                    <Text style={{fontSize: 24, alignSelf:'center'}}> Skorun---{'>'} {props.totalScore}</Text>
+                    <Text>BİLGİ</Text>
+
                 </>
                 :
                 <>
-                    <Text>GEÇEMEDİN</Text>
-                    <Text></Text>
-                    <Text> Puanın={'>'} {props.totalScore}</Text>
+                    <Text style={{fontSize: 24, alignSelf:'center'}}>GEÇEMEDİN</Text>
+                    <Text style={{fontSize: 22, alignSelf:'center'}}> Skorun---{'>'} {props.totalScore}</Text>
+                    <Text>BİLGİ</Text>
+                    <View style={{flex:1, justifyContent:'space-around', flexDirection:'row', alignItems:'flex-end'}}> 
+                        <TouchableOpacity style={{flex:0.3,  borderColor: 'red', borderWidth: 1, borderRadius: 3}}>
+                            <Text style={{color:'purple', alignSelf:'center'}}>1 HAK DAHA KAZAN</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{flex:0.3, borderColor: 'red', borderWidth: 1, paddingVertical: Metrics.height* 0.01}}>
+                            <Text style={{color:'purple', alignSelf:'center'}}>ANA MENÜ</Text>
+                        </TouchableOpacity>
+                    </View>
                 </>
             }
         </View>

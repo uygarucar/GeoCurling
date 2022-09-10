d53c2ff7a9aa7097f792cb1a8074416ac563a12d
Uygar Uçar <uygarucar2@gmail.com>
2022-09-10 22:38:18 +0300

    "Türkiye İklimi" Unit added rawly

---
+++ src/Modules/Play/Data/TargetPlacesInfos_Unit3.js
@@ -0,0 +1,85 @@
+//TÜRKİYE İKLİMİ
+
+//AŞağıdaki yorumları henüz kullanmadım.
+
+//Keşişlemenin(Samyeli) geldiği Keşiş dağı hangi dağdır?      //Bursa uludağ
+//Yıl boyu yağış + yıkanmış, asitli toprak isteyen sebzenin en cok yetiştiği il //Çay Rize
+
+//Mutlak nemin en fazla olduğu bölge
+//---------
+//Mutlak nemin maksimum olduğu bölgemizdeki... (Hava içerisindeki nem en fazla Akdeniz'de fazladır.) 
+//Sulu + Sıcak bölge olmalı
+//---------
+//Yazın Maksimum nemin en fazla olduğu bölgemiz -> Havanının alabileceği en yüksek nem miktarı: KURU + SICAK (GDA)
+//Bu nedenle Güneydoğu anadolu
+//---------
+//Bağıl Nemin en fazla olduğu bölgemiz:  Havanın neme doymasından dolayı yağmurun yağmasına neden olur
+//Bu nedenle yağmurlu yerlerde en yüksektir. ----> Karadeniz
+
+
+//Yazın en yağışlı bölge : Kuzeydoğu ANadolu
+//Sonbahar en yağışlı bölge(mizdeki...): Karadeniz
+//İlkbaharda en yağışlı bölgemiz: İç anadolu
+//Kışın en yağışlı bölgemiz. : Akdeniz ve Ege
+
+//İlkbaharda en az yağışlı bölgemiz: Karadeniz
+//Kışın en AZ yağışlı bölgemiz. :kuzeydoğu anadolu
+export const İklimler = [
+    {
+        //Karaca Dağ
+        id: 1,
+        text: 'ENlem, Karasallık ve sıcak rüzgarlar sebebiyle en sıcak yazın yaşandığı bölgemizdeki bir ...',  //Sıcaktan EKS oldu. Enlem, Karasallık, SIcak rüzgarlar
+        latitude: 37.6889, //GDA'da nere olabilir,
+        longitude: 43.1047
+    },
+    {
+        //Meke Maarı
+        id: 2,
+        text: 'Enlem ve Nemlilik sebepleriyle Türkiye\'de en sıcak Kışın yaşandığı bölgemizdeki bir...',
+        latitude: 37.688716932396964, //Akdenizde Anamur olması lazım ???
+        longitude: 33.63490922641875
+    },
+    {
+        //Ağrı
+        id: 3,
+        text: 'Yazın, Yükselti sebebiyle en soğuk olan bölgemizdeki bir.... ', //Kuzeydoğu anadolu
+        latitude: 39.71944,
+        longitude: 43.05139
+    },
+    {
+        //Bursa Uludağ
+        id: 4,
+        text: 'Yazın; Enlem, Karasallık, Sibirya basıncı, Yükselti sebepleriyle en soğuk olan bölgemizdeki bir...',//Kuzeydoğu anadolu
+        latitude: 40.08726293176254,
+        longitude: 29.213647872546154
+    },
+]
+
+export const Mikroklima = [
+    {
+        id: 1,
+        text: 'Tropikal ortama özgü ancak ülkemizde mikroklima olarak da yetişen bir ürününün en çok yetiştiği ilimiz',
+        latitude: 39.71944,//ANtalya veya mersin koordinatlarını koy
+        longitude: 43.05139//Muz
+    },
+    {
+        id: 2,
+        text: 'En çok yağışı kışın alan bölgemize özgü olan bir ürünün mikro klima olarak yetiştirildiği bir il',
+        latitude: 36.7477,//Zeytini kastediyor
+        longitude: 36.3327//Artvin
+    }
+    ,
+    {
+        id: 3,
+        text: 'Fön rüzgarı sayesinde Turunçgilin mikro klima olarak yetiştiği ilimiz',
+        latitude: 40.587580134,//Rize
+        longitude: 37.178795716,
+    },
+    {
+        id: 4, //Pamuk
+        text: 'Yaz kuraklığına sahip olması sebebiyle Pamuk\'un mikroklima olarak yetişebildiği ilimiz',
+        latitude: 40.587580134, //Iğdır
+        longitude: 37.178795716,
+    }
+]
+

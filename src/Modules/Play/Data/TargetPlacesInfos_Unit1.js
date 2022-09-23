093964011e0da533c894baa9f94d0c019e3aa13d
Uygar Uçar <uygarucar2@gmail.com>
2022-09-23 14:44:32 +0300

    TargetPlaces1 content improved

--- src/Modules/Play/Data/TargetPlacesInfos_Unit1.js
+++ src/Modules/Play/Data/TargetPlacesInfos_Unit1.js
@@ -1,100 +1,103 @@
 export const Daglar = [
-    {
+    {   //Süphan
         id: 1,
-        text: 'Kar Yağışı ve Donma olaylarının en yüksek olduğu bölgemizdeki bir volkanik dağ',
-        latitude: 39.71944,
-        longitude: 43.05139,
-        solution:'Süphan Dağı \n\n Kar yağışı ve donma olayları en çok \n Doğu Anadolu bölgesinde görülür Burada yer alan volkanik dağımız ise Süphan\'dır'
-
+        text: 'Kar Yağışı ve Donma olaylarının en çok görüldüğü bölgemizde yer alan bir volkanik dağ.',
+        latitude: 38.93984058000923,
+        longitude: 42.834719025567516,
+        solution:'Süphan Dağı \n\n Doğu Anadolu bölgesindeki volkanik dağlardan birini kastediyor. \n\n Süphan Dağı, bu bölgede yer alan volkanik dağlarımızdan biridir.'
+         
     },
-    {
+    {//Amanos/ Nur
         id: 2,
-        text: 'Ege bölgesinde olmayan tek Horst dağı',
+        text: 'Ege bölgesinde yer almayan tek Horst dağı',
         latitude: 36.7477,
         longitude: 36.3327,
-        solution: 'Amanos/ Nur Dağları \n\n Horst, kırık dağlara verilen isimdir.\n Ege bölgesinde olmayan tek kırık dağ ise Hatay\'da yer alan Amanos(Nur) dağ(ları)dır '
+        solution: 'Amanos/ Nur Dağları \n\n Horst, kırık dağlara verilen isimdir.\n Ege bölgesinde yer almayan tek kırık dağ ise Hatay\'daki Amanos(Nur) dağlarıdır '
     }
     ,
-    {
+    {//Canik dağları 
         id: 3,
-        text: 'Alçak olması nedeniyle Karadeniz ikliminin iç kesimlere etki etmesini kolaylaştıran dağ',
+        text: 'Alçak olması sebebiyle Karadeniz ikliminin iç kesimlere etki etmesini kolaylaştıran dağ',
         latitude: 40.587580134,
         longitude: 37.178795716,
-        solution: 'Canik Dağları \n\n Tarif edilen dağ(lar), orta karadeniz bölgesinde yer alan Canik dağlarıdır.'
+        solution: 'Canik Dağları \n\n Tarif edilen dağ, Orta Karadeniz bölgesinde yer alan Canik dağlarıdır.'
     },
 ]
 export const Volkanizma = [
     {
-        //Karaca Dağ
+        //Dİyarbakır, Mardin, Şanlıurfa - Karaca Dağ 
         id: 1,
-        text: 'Türkiye\'nin tek kalkan dağı',
-        latitude: 37.6889,
-        longitude: 43.1047
+        text: 'Türkiye\'nin tek kalkan dağının yer aldığı illerimizden birine git',
+        latitude: 37.927932054851794,
+        longitude: 40.221105742710606,
+        solution:'Diyarbakır \n\n Tek kalkan dağı olan Karaca dağ, \n Diyarbakır, Mardin, Şanlıurfa sınırları içinde yer alır. Gidilmesi istenen il ise Diyarbakır\'dır.'
     },
     {
         //Meke Maarı
         id: 2,
-        text: 'Dünyanın nazar boncuğu olarak bilinen Meke Maarı nerededir?',
-        latitude: 37.688716932396964,
-        longitude: 33.63490922641875
-    },
-    {
-        //Ağrı
-        id: 3,
-        text: 'Kar Yağışı ve Donma olaylarının en yüksek olduğu bölgemizdeki bir volkanik dağ',
-        latitude: 39.71944,
-        longitude: 43.05139
+        text: 'Dünyanın nazar boncuğu olarak bilinen Meke Maarı hangi ilimizde yer alır?',
+        latitude: 38.12343792297119, 
+        longitude: 32.47662986268032,
+        solution: 'Konya \n\n Meke Maarı, Konya\'da yer alır.'
+        
     },
     {
         //Bursa Uludağ
-        id: 4,
-        text: 'Türkiye\'deki tek batolit örneği',
+        id: 3,
+        text: 'Türkiye\'deki tek batolit örneği hangi dağımızdır?',
         latitude: 40.08726293176254,
-        longitude: 29.213647872546154
+        longitude: 29.213647872546154,
+        solution: 'Bursa - Uludağ \n\n Türkiye\'nin tek batoliti, Bursa Uludağ\'dır.'
     },
 ]
 export const MasifAraziler = [
-    {
-        id: 1,
-        text: '4. Jeolojik zaman volkanik arazilerinin bulunduğu bölgemizdeki bir masif arazi!',
-        latitude: 38.254,
-        longitude: 27.134
+    {   // Yıldız/Istranca masifi Kırklarelinde yer almaktadır.
+        text: 'Yıldız/Istranca masif arazisi hangi ilimizde yer almaktadır?',
+        latitude: 41.761801639892454,
+        longitude: 27.208274562258282,
+        solution: 'Kırklareli \n\n Yıldız/Istranca masifi Kırklareli\'nde yer almaktadır.'
     },
-    {
+    { //Bitlis masifini kastediyor. 
         id: 2,
         text: 'Yükseltisi en fazla olan bölgemizdeki bir masif arazi',
-        latitude: 38.40115,
-        longitude: 42.10784
+        latitude: 38.43592465615338, 
+        longitude: 42.12369281646592,
+        solution: 'Bitlis Masifi \n\n Yükseltisi en fazla olan bölgemiz Doğu Anadolu bölgesidir. Bu bölgedeki masif arazi Bitlis Masifidir.'
     }
     ,
-    {
+    {   //Mersin/Anamur Masifini kastediyor
         id: 3,
-        text: 'Kış sıcaklığının en fazla olduğu bölgemizdeki bir masif arazi',
+        text: 'Kış sıcaklığının en fazla olduğu bölgemizdeki yer alan masif arazi arazi hangi ilimizdedir?',
         latitude: 36.077286,
         longitude: 32.832890,
+        solution: 'Mersin/Anamur \n\n Kış sıcaklığı Akdeniz Bölgesi\'nde en fazladır. Buradaki masif arazi Mersin Anamur masifidir.'
     }
 ]
 
 export const Platolar = [
     {
-        //Kocaeli
+        //Çatalca - Kocaeli Platosu
         id: 1,
         text: 'En yoğun nüfuslu aşınım düzlüğü platosu',
         latitude: 40.945317723668275,
-        longitude: 29.5579235653009
+        longitude: 29.5579235653009,
+        solution:'Çatalca - Kocaeli Platosu \n\n En yoğun nüfusa sahip plato Çatalca - Kocaeli platosudur.'
+
     },
-    {
+    {   //Karaca dağ
         id: 2,
         text: 'En geniş yer kaplayan plato hangi ilimizdedir?',
         latitude: 39.755630120802216,
-        longitude: 34.84885581785901
+        longitude: 34.84885581785901,
+        solution: 'Yozgat \n\n Ülkemizde en geniş yeri kaplayan plato Yozgat\'taki - Bozok Platosu\'dur.'
     }
     ,
-    {
+    {   //Konya  
         id: 3,
-        text: 'İsmini aldığı Obruk şekillerden meydana gelen plato hangi ilimizdedir?',
-        latitude: 37.606307017772515,
-        longitude: 33.59706017738192,
+        text: 'Obruk platosu hangi ilimizdedir?',
+        latitude: 37.88786413399014,
+        longitude: 32.485048632908764,
+        solution: 'Konya \n\n Obruk platosu, Konyada yer almaktadır.'
     }
 ]
 
@@ -104,22 +107,25 @@ export const FayHatları = [
         id: 1,
         text: 'Türkiye\'de gerçekleşen en büyük deprem nerede oldu?',
         latitude: 39.74588032538926,
-        longitude: 39.479199134379584
+        longitude: 39.479199134379584,
+        solution: 'Erzincan Depremi - 1939'
     },
     {
         //Bingöl
         id: 2,
         text: 'Türkiye\'de en tehlikeli 2 fay hattının kesişme noktası hangi ilimizdir?',
         latitude: 38.885349,
-        longitude: 40.498291
+        longitude: 40.498291,
+        solution: 'Bingöl \n\n Kuzey Anadolu Fay hattı ile Doğu Anadolu Fay hattının birleştiği yer Bingöl\'dür.'
     }
     ,
     {
-        //Mersin
+        //Mersin - Taşeli Platosunda deprem olmaz.
         id: 3,
-        text: 'Fay hatlarından en uzakta Akdeniz bölgesi ili neresidir?',
+        text: 'Fay hatlarından en uzakta olan Akdeniz bölgesi ili nerededir?',
         latitude: 36.81110790740455,
         longitude: 34.64146149178453,
+        solution: 'Mersin - Taşeli Platosu\n\n Bölge olarak Taşeli Platosu, il olarak Mersin, deprem riski en az olan Akdeniz Bölgesi bölümleridir.'
     },
 ]
 
